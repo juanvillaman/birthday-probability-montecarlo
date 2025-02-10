952e0a7bb3565d14dfd1f654aeae9cd79ad7f3a4
juanvillaman <juandanielvill29@gmail.com>
2025-02-10 12:48:13 -0500

    Created Person object constructor
    Months and days are assigned correctly, taking care of months with 30 or 31 days

---
+++ BirthdayProgram/src/Person.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Person {
+  
+  private int month;
+  private int day;
+
+  Random rand = new Random();
+
+  public Person(){
+    month = rand.nextInt(12) + 1;
+
+    if (month == 2){
+      day = rand.nextInt(28) + 1;
+    }
+    else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+      day = rand.nextInt(31) + 1;
+    }
+    else {
+      day = rand.nextInt(30) + 1;
+    }
+  }
+}
+
+
